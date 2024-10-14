86739b9685966dcd09d8ce39163f695d050084bb
Xinyi Li <wolixinyi@gmail.com>
2024-10-14 04:29:22 -0700

    init

---
+++ streamlit_monaco_code_editor/frontend/src/MonacoEditorComponent.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useRef} from 'react';
+import Editor from '@monaco-editor/react';
+// import * as monaco from "monaco-editor";
+import { withStreamlitConnection, Streamlit, ComponentProps } from 'streamlit-component-lib';
+
+const lineHeight = 20;
+
+const MonacoEditorComponent = ({ args, width, disabled, theme }: ComponentProps) => {
+  let vs_theme = 'vs-dark';
+  if ( theme!==undefined && (theme.base === 'light') ) {
+    vs_theme = 'vs-light';
+  }
+  const editorRef = useRef<any>(null);
+  const minLines = args.minLines || 10;
+  const [height, setHeight] = React.useState(lineHeight * minLines);
+
+  useEffect(() => {
+    Streamlit.setFrameHeight();
+  }, [height]);
+
+  const countLines = (value: string) => {
+    return value.split('\n').length;
+  }
+
+  const onChange = (value: string|undefined, event: any) => {
+    const editor = editorRef.current;
+    Streamlit.setComponentValue(value);
+    const lineCount = countLines(value || '');
+    setHeight(lineHeight * Math.max(minLines, lineCount));
+    console.log('height', height);
+    // Streamlit.setFrameHeight(height);
+    editor.layout();
+  }
+
+  return (
+    <Editor
+      width={width}
+      height={height}
+      theme={vs_theme}
+      language={args.language}
+      defaultValue="# Write your Python code here"
+      onChange={onChange}
+      options={{
+        automaticLayout: true,
+        minimap: {enabled: false},
+        lineHeight: lineHeight,
+      }}
+      onMount={(editor, monaco) => {
+        editorRef.current = editor;
+        const editorElement = editor.getDomNode()!.parentElement;
+        console.log('editorElement', editorElement);
+        editor.layout();
+      }}
+    />
+  );
+
+};
+
+export default withStreamlitConnection(MonacoEditorComponent)
---
+++ streamlit_monaco_code_editor/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import MonacoEditorComponent from './MonacoEditorComponent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MonacoEditorComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
